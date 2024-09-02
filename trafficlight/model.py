5ac84bfb11eb02fddbab0ec76c044bca0cbcdf4a
Furtif <msntchat@hotmail.fr>
2024-09-02 16:02:15 +0200

    fix commit 0b31ee9b inofficial support for golbat raws

--- trafficlight/model.py
+++ trafficlight/model.py
@@ -1,14 +1,26 @@
 from pydantic import BaseModel, Field
+from .config import config
 
 
 class ProtoModel(BaseModel):
-    method: int = Field(alias="type")
-    request: str | None
-    response: str | None = Field(alias="payload")
+    if config.use_golbat_raws:
+        method: int = Field(alias="type")
+        request: str | None
+        response: str | None = Field(alias="payload")
+    else:
+        method: int
+        request: str | None
+        response: str | None
 
 
 class RequestModel(BaseModel):
-    rpcid: int = 0
-    rpchandle: int | None
-    rpcstatus: int = 0
-    protos: list[ProtoModel] = Field(alias="contents")
+    if config.use_golbat_raws:
+        rpcid: int = 0
+        rpchandle: int | None
+        rpcstatus: int = 0
+        protos: list[ProtoModel] = Field(alias="contents")
+    else:
+        rpcid: int
+        rpchandle: int | None
+        rpcstatus: int
+        protos: list[ProtoModel]
