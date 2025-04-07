03a8639c28f6568d8b373f037770a02c23cb263e
Fabio1988 <fabio.schett@gmail.com>
2025-04-07 19:54:10 +0200

    feat: allow poupulation by field name

--- trafficlight/model.py
+++ trafficlight/model.py
@@ -6,9 +6,14 @@ class ProtoModel(BaseModel):
     request: str | None
     response: str | None = Field(alias="payload")
 
+    class Config:
+        allow_population_by_field_name = True
 
 class RequestModel(BaseModel):
     rpcid: int = 0
     rpchandle: int | None
     rpcstatus: int = 0
     protos: list[ProtoModel] = Field(alias="contents")
+
+    class Config:
+        allow_population_by_field_name = True
